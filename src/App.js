1f142e3a8b7f74e6ec8e5f33a7067a7561a7499b
BenPot <pot.94.handig@gmail.com>
2021-08-07 12:26:59 +0800

    making multiple faces detection

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ class App extends Component {
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
             />
-            <FaceRecognition box={box} imageUrl={imageUrl}/>
+            <FaceRecognition boxes={boxes} imageUrl={imageUrl}/>
           </div>
           : ( this.state.route !== 'register' ?
               <Signin appURL={appURL} loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, boxes }) => {
 	return (
 		<div className='center ma'>
 			<div className='absolute mt2'>
 				<img id='inputImage' alt='' src={imageUrl} width='500px' height='auto'/>
-				<div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+				<div 
+					className='bounding-box'
+					style={{top: boxes[0].topRow, right: boxes[0].rightCol, bottom: boxes[0].bottomRow, left: boxes[0].leftCol}}>
+				</div>
+				<div 
+					className='bounding-box'
+					style={{top: boxes[1].topRow, right: boxes[1].rightCol, bottom: boxes[1].bottomRow, left: boxes[1].leftCol}}>
+				</div>
 			</div>
 		</div>
 	);
